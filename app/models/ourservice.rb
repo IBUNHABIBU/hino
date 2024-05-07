733ef243a010e9ca69576d66bf0f0b85963d489c
IBUNHABIBU <ibunhabib@gmail.com>
2024-05-07 22:55:13 +0300

    Adding pdf viewer

--- app/models/ourservice.rb
+++ app/models/ourservice.rb
@@ -1,5 +1,6 @@
 class Ourservice < ApplicationRecord
     has_many_attached :images
+    has_many_attached :pdf_files
     belongs_to :user
     validates :Title, presence: true, length: { minimum: 5 }
     validates :description, presence: true, length: { minimum: 10 }
