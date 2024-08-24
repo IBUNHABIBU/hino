dc279219896ce73e45224e5cebb40c8df2c32655
IBUNHABIBU <ibunhabib@gmail.com>
2024-08-24 08:43:18 +0300

    Update gems

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
     address: "smtp.gmail.com",
     port: 587,
     domain: "example.com",
-    user_name: Rails.application.credentials.gmail_name],
+    user_name: Rails.application.credentials.gmail_name,
     password: Rails.application.credentials.gmail_pswd,
     authentication: "plain",
     enable_starttls_auto: true
