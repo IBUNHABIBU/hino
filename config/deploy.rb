2ce4e9e2845697b1330bf59dc6a3ad30d963dd00
IBUNHABIBU <ibunhabib@gmail.com>
2024-09-01 08:55:43 +0300

    Update deployment configurations

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,9 +20,21 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bund
 set :branch, ENV['BRANCH'] || 'main'
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
+set :rails_env, 'production'
 
 set :log_level, :debug
 
+set :puma_threads, [4, 16]
+set :puma_workers, 2
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    # Accept array for multi-bind
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_access_log, "#{shared_path}/log/puma.error.log"
+set :puma_error_log, "#{shared_path}/log/puma.access.log"
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
