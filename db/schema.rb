199d27d6576242a65d0b485870a1c72ad7309843
IBUNHABIBU <ibunhabib@gmail.com>
2024-12-25 21:00:25 +0300

    Add friendly id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_12_210726) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_25_175938) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -65,6 +65,8 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_12_210726) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "slug"
+    t.index ["slug"], name: "index_ourservices_on_slug", unique: true
   end
 
   create_table "partners", force: :cascade do |t|
