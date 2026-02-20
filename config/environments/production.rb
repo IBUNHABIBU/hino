08347858b5b0d0982627d81d0b772b0ff0ccb721
IBUNHABIBU <ibunhabib@gmail.com>
2026-02-20 22:51:49 +0300

    Add docker

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
+  # config.action_mailer.default_url_options = { host: "example.com" }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
   # config.action_mailer.smtp_settings = {
@@ -67,6 +67,20 @@ Rails.application.configure do
   #   port: 587,
   #   authentication: :plain
   # }
+
+  config.action_mailer.default_url_options = { host: "kilimarvellous.com", protocol: "https" }
+
+config.action_mailer.delivery_method = :smtp
+
+config.action_mailer.smtp_settings = {
+  address:              "smtp.gmail.com",
+  port:                 587,
+  domain:               "kilimarvellous.com",
+  user_name:            ENV["SMTP_USERNAME"], # better to store in credentials or env vars
+  password:             ENV["SMTP_PASSWORD"],
+  authentication:       "plain",
+  enable_starttls_auto: true
+}
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
