05ae9ebdaf062f2f4bd66c8f6592193a60fcee99
IBUNHABIBU <ibunhabib@gmail.com>
2026-02-22 21:29:27 +0300

    Add solid queue

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,6 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  get "up" => "rails/health#show", as: :rails_health_check
   root "home#index"
 end
