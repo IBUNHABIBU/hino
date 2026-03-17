ed564bb5c0c74e408bea16d2046c4a199f748c2c
IBUNHABIBU <ibunhabib@gmail.com>
2026-03-17 13:37:42 +0300

    Add nav helpers

--- app/helpers/nav_helper.rb
+++ app/helpers/nav_helper.rb
@@ -37,4 +37,93 @@ module NavHelper
   def mega_menu?(item_name)
     ['Service', 'Company'].include?(item_name)
   end
+  # ... existing nav_menu_items and other methods ...
+
+  # Quick Links for Footer
+  def quick_links
+    [
+      { name: 'Home', path: 'root_path' },
+      { name: 'Services', path: 'ourservices_path' },
+      { name: 'Company', path: 'about_us_path' },
+      { name: 'Contact Us', path: 'contact_us_path' },
+    ]
+  end
+
+  # Footer Contact Information
+  def footer_contact_info
+    {
+      address: {
+        icon: 'fa-location-dot',
+        line1: 'Sido Shinyanga, Tabora Road',
+        line2: 'Ibinzamata Area, Plot no. 2, Block Y',
+        line3: 'P. O. BOX 8804, Shinyanga'
+      },
+      phone: {
+        icon: 'fa-phone-volume',
+        number: '+255 685325766',
+        display: '0685325766'
+      },
+      email: {
+        icon: 'fa-envelope',
+        address: 'directors@ecohygiene.co.tz'
+      },
+      hours: {
+        icon: 'fa-clock',
+        text: '24/7',
+        label: 'OFFICE HOURS'
+      }
+    }
+  end
+
+  # Latest Projects
+  def latest_projects
+    [
+      { name: 'Opening of new mixing setup at Shinyanga region', date: '2024' },
+      # Add more projects here as they come
+    ]
+  end
+
+  # Footer Sections Configuration
+  def footer_sections
+    [
+      {
+        title: 'INTERESTING INFOS',
+        type: 'text',
+        content: 'We seek to survive market conditions and contribute immensely to the economic growth of the country.'
+      },
+      {
+        title: 'QUICK LINKS',
+        type: 'links',
+        content: quick_links
+      },
+      {
+        title: 'ECOHYGIENE COMPANY LTD',
+        type: 'address',
+        content: footer_contact_info
+      },
+      {
+        title: 'LATEST PROJECTS',
+        type: 'projects',
+        content: latest_projects
+      }
+    ]
+  end
+
+  # Footer Social Links
+  def footer_social_links
+    [
+      { platform: 'facebook', url: 'https://facebook.com', icon: 'fa-facebook', label: 'Follow us on Facebook' },
+      { platform: 'twitter', url: 'https://twitter.com', icon: 'fa-twitter', label: 'Follow us on Twitter' },
+      { platform: 'linkedin', url: 'https://linkedin.com', icon: 'fa-linkedin', label: 'Follow us on LinkedIn' },
+      { platform: 'instagram', url: 'https://instagram.com', icon: 'fa-instagram', label: 'Follow us on Instagram' }
+    ]
+  end
+
+  # Copyright Information
+  def copyright_info
+    {
+      text: 'Copyright © 2024 ECOHYGIENE, All Rights Reserved.',
+      link: '#'
+    }
+  end
 end
